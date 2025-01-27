22b1fa7869d6e14aecd467d40204984585b54b17
Thauan Nery <tpcn2000@gmail.com>
2025-01-27 10:07:23 -0300

    css da tela de movies e model de filmes

---
+++ models/movie.php
@@ -0,0 +1,31 @@
+<?php
+
+    class Filmes{
+
+        public $id;
+        public $title;
+        public $description;
+        public $image;
+        public $trailer;
+        public $category;
+        public $length;
+        public $usuario_id;
+
+        public function imageGenerateName()
+        {
+            return bin2hex(random_bytes(60)) . "jpg";
+        }
+    }
+
+        interface MovieDaoInterface{
+            public function BuildMovie($data);
+            public function FindAll();
+            public function getLatestMovies();
+            public function getMovieByCategory($category);
+            public function getMoviesByUserId($id);
+            public function FinById($id);
+            public function findByTitle($title);
+            public function create(movie $movie);
+            public function update(movie $movie);
+            public function destroy($id);
+        }
